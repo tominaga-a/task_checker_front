4c85743d9fde94ec2bd1232780cf5d23c42d428a
LAKEEL\tominaga-a <tominaga-a@lakeel.com>
2026-03-09 11:27:19 +0900

    ユーザー新規登録機能の実装

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,17 +1,31 @@
 <script setup>
 import Header from './Header.vue'
 import { ref } from 'vue' 
-import { auth, createUserWithEmailAndPassword } from '../firebase'; 
+import { auth, createUserWithEmailAndPassword} from '../firebase'; 
 import { useRouter } from 'vue-router';
+const email = ref(''); 
+const password = ref('')
+
+const router = useRouter();
+
+const handleSignUp = async() => {
+  try {
+    await createUserWithEmailAndPassword(auth, email.value, password.value)
+    router.push("/home");
+  }catch(error) {
+    console.log('ユーザー登録できませんでした', error)
+  }
+}
+
 </script>
 
 <template>
   <Header />
   <div class="form-body">
     <h1>新規登録</h1>
-    <input type="text" id="email"  placeholder="email">
-    <input type="password" id="password"placeholder="password">
-    <button value="新規登録">新規登録</button>
+    <input type="text" id="email" v-model="email" placeholder="email">
+    <input type="password" id="password" v-model="password" placeholder="password">
+    <button value="新規登録" @click="handleSignUp">新規登録</button> 
      <p>既にアカウントをお持ちの方はこちらへ<router-link to="/">こちら</router-link></p>
   </div>
 </template>
